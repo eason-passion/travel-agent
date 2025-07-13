ada5758f0b2306bdd404027ecfd4aaac2a6a3bfc
YHH <1371356187@qq.com>
2025-07-13 14:45:49 +0800

    feat: 后端跨域配置

---
+++ src/main/java/com/yhh/travelagent/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.yhh.travelagent.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Date 2025-07-13 0:32
+ * @ClassName: CorsConfig
+ * @Description: 全局跨域配置
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 覆盖所有请求
+        registry.addMapping("/**")
+                // 允许发送 Cookie
+                .allowCredentials(true)
+                // 放行哪些域名（必须用 patterns，否则 * 会和 allowCredentials 冲突）
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+}
